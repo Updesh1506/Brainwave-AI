f9dc078dbf3e6a4c62d9c9883e603234f7e2d95f
Updesh Yadav <updeshdeepu1506@gmail.com>
2024-05-17 23:24:14 +0530

    undo what i have done

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-};
-
+export default defineConfig({
+  plugins: [react()],
+})
 
