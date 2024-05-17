c34feccfd08af799e6d4eced8ff80a4c95bf43db
Updesh Yadav <updeshdeepu1506@gmail.com>
2024-05-17 23:38:27 +0530

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
-
+      external: ['scroll-lock']
+    }
+  }
 })
-
